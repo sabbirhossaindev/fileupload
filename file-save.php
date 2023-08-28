8ba1e7bf6b9f3e1a1b11c0ecba49ae6c4715a43b
sabbir hossain <mdsabbir477470@gmail.com>
2023-08-28 21:05:02 +0600

    file update done

--- file-save.php
+++ file-save.php
@@ -38,25 +38,47 @@ echo"Do you speak any languages other than English: " .  $_POST['language'];
 echo "<br>";
 
 // upload file......
-// var_dump($_FILES['photo']); // check data 
-
-function imageUpload($file_form_name, $jpg, $png, $jpeg){
-    $file_name = $file_form_name['name'];
-    $file_size = $file_form_name['size'];
-    // echo "File Size ".$file_size;
-    // exit();
-    $file_extension = explode(".", $file_name);
-    $file_type = $file_extension[1];
-    if($file_type == $jpg || $file_type == $png || $file_type == $jpeg && $file_size == "2. MB"){
-        $file_tmp = $file_form_name['tmp_name'];
-        $file_destination = 'uploads/'.$file_name;
-        move_uploaded_file($file_tmp,$file_destination);
-        return "<h2>File uploaded done!</h2>";
-    }
-    else{
-        return "<h2>File not match. Please try again!</h2>";
+$target_directory = "uploads/";
+$target_file = $target_directory . basename($_FILES["fileUpload"]["name"]);
+$fileuploadDone = 1;
+$fileExtension = pathinfo($target_file,PATHINFO_EXTENSION);
+// ফাইলটি ছবি কিনা তা চেক করে
+if(isset($_POST["submit"])) {
+    $check = getimagesize($_FILES["fileUpload"]["tmp_name"]);
+    if($check !== false) {
+        echo	"ফাইলটি ইমেজ - " . $check["mime"] . ".";
+        $fileuploadDone = 1;
+    } else {
+        echo "ফাইলটি ইমেজ নয়।";
+        $fileuploadDone = 0;
     }
 }
-imageUpload($_FILES['photo'],"jpg", "png", "jpeg", "2. MB");
+// ফাইলটি ইতিমধ্যে বিদ্যমান কিনা তা চেক করে
+if (file_exists($target_file)) {
+    echo "দুঃখিত, ফাইলটি পূর্ব থেকেই আপলোড রয়েছে।";
+    $fileuploadDone = 0;
+}
+// ফাইলের সাইজ চেক
+if ($_FILES["fileUpload"]["size"] > 500000) {
+    echo "দুঃখিত, আপনার ফাইলটি অনেক বড় আকারের।";
+    $fileuploadDone = 0;
+}
+// কয়েকটি নির্দিষ্ট টাইপের ফাইল আপলোডের অনুমতি
+if($fileExtension != "jpg" && $fileExtension != "png" && $fileExtension != "jpeg"
+&& $fileExtension != "gif" ) {
+    echo "দুঃখিত, শুধুমাত্র JPG, JPEG, PNG এবং GIF ফাইল আপলোড করা যাবে।";
+    $fileuploadDone = 0;
+}
+// $fileuploadDone কি error এর কারনে 0 তে সেট হয়েছে কিনা তা চেক
+if ($fileuploadDone == 0) {
+    echo "দুঃখিত, আপনার ফাইলটি আপলোড হয়নি।";
+// যদি সবকিছু ঠিক থাকে, তাহলে ফাইল আপলোড করা।
+} else {
+    if (move_uploaded_file($_FILES["fileUpload"]["tmp_name"], $target_file)) {
+        echo " ". basename( $_FILES["fileUpload"]["name"]). " ফাইলটি আপলোড হয়েছে। ";
+    } else {
+        echo "দুঃখিত, আপনার ফাইলটি আপলোডে সমস্যা হচ্ছে।";
+    }
+}
 
 ?>
--- file-upload-system.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-// var_dump($_FILES['photo']);
-// include('file-upload-system.php');
-// echo imageUpload($_FILES['photo'],"jpg", "png", "jpeg", "2. MB");
-
-?>
